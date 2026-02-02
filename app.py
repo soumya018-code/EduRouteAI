5696c299a70e44508e7b7daf16aadf1c85a0b3dd
soumya018-code <scycosoumya@gmail.com>
2026-02-02 12:06:02 +0530

    render config

--- app.py
+++ app.py
@@ -101,5 +101,6 @@ def chatbot():
         reply="Ask about careers, colleges, or courses."
     return jsonify({"reply":reply})
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+
